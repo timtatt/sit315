6142860b414bf130a9e145b7cceaa88d61e1066f
Tim Tattersall <tim@tattsy.com>
2019-04-29 20:58:52 -0700

    Redoing 2.1P after source code was lost when VM went missing

---
+++ task2pt1/matrices.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<pthread.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<math.h>
+
+const int matrixSize = 200;
+const int numThreads = 4;
+
+int rowsPerThread = floor(matrixSize / numThreads);
+int a[matrixSize][matrixSize];
+int b[matrixSize][matrixSize];
+int c[matrixSize][matrixSize];
+
+using namespace std;
+
+void* calculateRow(void* threadArg) {
+    long threadId = (long) threadArg;
+
+    int lowerBound = (threadId * rowsPerThread);
+    int upperBound = (threadId * rowsPerThread) + rowsPerThread;
+
+    if (upperBound > matrixSize) {
+        upperBound = matrixSize;
+    }
+
+    for (int i = lowerBound; i < upperBound; i++) {
+        for (int j = 0; j < matrixSize; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < matrixSize; k++) {
+                c[i][j] += a[i][j] * b[k][j];
+            }
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+
+int main() {
+    // Generate Matrices;
+    int seed = 10;
+    clock_t timer;
+    double timeTaken = 0;
+    pthread_t threads[numThreads];
+
+    srand(seed);
+
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize; j++) {
+            a[i][j] = rand() % 10 + 1;
+            b[i][j] = rand() % 10 + 1;
+        }
+    }
+
+
+    timer = clock();
+
+    // Sequential Matrix
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < matrixSize; k++) {
+                c[i][j] += a[i][j] * b[k][j];
+            }
+        }
+    }
+
+    timeTaken = (clock() - timer) / (double) CLOCKS_PER_SEC;
+    cout << "Sequential Time Taken: " << timeTaken << "s\n";
+
+    timer = clock();
+
+    // Threaded Matrix
+    for (long i = 0; i < numThreads; i++) {
+        pthread_create(&threads[i], NULL, calculateRow, (void*) i);
+    }
+
+    for (long i = 0; i < numThreads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    timeTaken = (clock() - timer) / (double) CLOCKS_PER_SEC;
+    cout << "Parallel Time Taken: " << timeTaken << "s\n";
+
+
+    return 0;
+}
